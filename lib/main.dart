647d2848c66e1c258680cab49d17379a4f7b4c0f
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-07-25 22:32:23 -0300

    Criação do arquivo BasePage e do menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:futinfo/src/pages/base_page.dart';
 import 'package:futinfo/src/pages/home/home_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
       ),
-      home: const HomePage(),
+      home: const BasePage(),
     );
   }
 }
---
+++ lib/src/pages/base_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:futinfo/src/pages/home/home_page.dart';
+
+class BasePage extends StatefulWidget {
+  const BasePage({super.key});
+
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
+  int currentIndex = 0;
+  final pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: PageView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: pageController,
+          children: [
+            const HomePage(),
+            Container(
+              color: Colors.red,
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+              pageController.jumpToPage(index);
+            });
+          },
+          backgroundColor: Colors.blue,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: "Favoritos",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
