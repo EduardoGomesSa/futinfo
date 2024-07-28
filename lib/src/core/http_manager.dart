1247003f739948fad4382dc4816c79f0a8193604
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-07-27 23:54:31 -0300

    Criação do método requestList

--- lib/src/core/http_manager.dart
+++ lib/src/core/http_manager.dart
@@ -34,4 +34,38 @@ class HttpManager {
       return {};
     }
   }
+
+  Future<Map> requestList({
+    required String url,
+    required String method,
+    Map? headers,
+    List? body,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+      });
+
+    Dio dio = Dio();
+
+    try {
+      Response response = await dio.request(
+        url,
+        options: Options(
+          headers: defaultHeaders,
+          method: method,
+        ),
+        data: body,
+        queryParameters: queryParameters,
+      );
+
+      return response.data;
+    } on DioException catch (error) {
+      return error.response?.data ?? {};
+    } catch (error) {
+      return {};
+    }
+  }
 }
