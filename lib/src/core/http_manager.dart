0d30d4ce4d2e8cde49ab8d51294c55e7497bfb2e
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-07-27 23:57:01 -0300

    Criação da class HttpMethods com os verbos das requisições

--- lib/src/core/http_manager.dart
+++ lib/src/core/http_manager.dart
@@ -69,3 +69,10 @@ class HttpManager {
     }
   }
 }
+
+abstract class HttpMethods {
+  static const String post = "POST";
+  static const String get = "GET";
+  static const String put = "PUT";
+  static const String delete = "DELETE";
+}
