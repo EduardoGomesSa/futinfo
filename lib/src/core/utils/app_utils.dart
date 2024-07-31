e239effa26ef208cc29ae196fb3e1b9820c31d58
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-07-31 20:10:38 -0300

    Criação da class AppUtils

---
+++ lib/src/core/utils/app_utils.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppUtils {
+  Future saveLocalData({
+    required String key,
+    required String data,
+  }) async {
+    final SharedPreferences storage = await SharedPreferences.getInstance();
+    await storage.setString(key, data);
+  }
+
+  Future<String?> getLocalData({required String key}) async {
+    final SharedPreferences storage = await SharedPreferences.getInstance();
+    return storage.getString(key);
+  }
+
+  Future removeLocalData({required String key}) async {
+    final SharedPreferences storage = await SharedPreferences.getInstance();
+    return await storage.remove(key);
+  }
+
+  String formatDateTime(DateTime dateTime) {
+    initializeDateFormatting();
+
+    DateFormat dateFormat = DateFormat.yMd('pt_BR').add_Hm();
+    return dateFormat.format(dateTime);
+  }
+
+  void showToast({required String message, bool isError = false}) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 4,
+      backgroundColor: isError ? Colors.red : Colors.green,
+      textColor: isError ? Colors.white : Colors.black,
+      fontSize: 18,
+    );
+  }
+}
