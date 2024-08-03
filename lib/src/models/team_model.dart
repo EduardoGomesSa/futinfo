33b308ee77f9dc7acb9bd67a7553964924785563
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-03 18:59:55 -0300

    Criação do TeamModel

---
+++ lib/src/models/team_model.dart
@@ -0,0 +1,15 @@
+class TeamModel {
+  final int? id;
+  final String? name;
+  final String? shortName;
+  final String? tla;
+  final String? crest;
+
+  TeamModel({
+    required this.id,
+    required this.name,
+    required this.shortName,
+    required this.tla,
+    required this.crest,
+  });
+}
