55038b4a9c5103e3caf4f19dd7b9ad0c29107ab1
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-03 18:34:32 -0300

    Criação do model Competition

---
+++ lib/src/models/competition_model.dart
@@ -0,0 +1,15 @@
+class CompetitionModel {
+  final int? id;
+  final String? name;
+  final String? code;
+  final String? type;
+  final String? flag;
+
+  CompetitionModel({
+    required this.id,
+    required this.name,
+    required this.code,
+    required this.type,
+    required this.flag,
+  });
+}
