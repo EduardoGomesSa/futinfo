4ab703bb746598d4a8ef5509a71ebfe19d033eb5
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-04 14:28:40 -0300

    Criação dos métodos de conversão de ScoreModel

--- lib/src/models/value_objects/score_model.dart
+++ lib/src/models/value_objects/score_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ScoreModel {
   final String? winner;
   final String? duration;
@@ -10,6 +12,15 @@ class ScoreModel {
     required this.fullTime,
     required this.halfTime,
   });
+
+  factory ScoreModel.fromMap(Map<String, dynamic> map) {
+    return ScoreModel(
+      winner: map['winner'] != null ? map['winner'] as String : null,
+      duration: map['duration'] != null ? map['duration'] as String : null,
+      fullTime: map['fullTime'] != null ? FullTime.fromMap(map['fullTime'] as Map<String, dynamic>) : null,
+      halfTime: map['halfTime'] != null ? HalfTime.fromMap(map['halfTime'] as Map<String, dynamic>) : null,
+    );
+  }
 }
 
 class FullTime {
@@ -20,6 +31,20 @@ class FullTime {
     required this.home,
     required this.away,
   });
+
+  factory FullTime.fromMap(Map<String, dynamic> map) {
+    return FullTime(
+      home: map['home'] != null ? map['home'] as String : null,
+      away: map['away'] != null ? map['away'] as String : null,
+    );
+  }
+
+  factory FullTime.fromJson(String source) =>
+      FullTime.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  static List<FullTime> fromList(list) {
+    return List<FullTime>.from(list.map((x) => FullTime.fromMap(x)));
+  }
 }
 
 class HalfTime {
@@ -30,4 +55,18 @@ class HalfTime {
     required this.home,
     required this.away,
   });
+
+  factory HalfTime.fromMap(Map<String, dynamic> map) {
+    return HalfTime(
+      home: map['home'] != null ? map['home'] as String : null,
+      away: map['away'] != null ? map['away'] as String : null,
+    );
+  }
+
+  factory HalfTime.fromJson(String source) =>
+      HalfTime.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  static List<HalfTime> fromList(list) {
+    return List<HalfTime>.from(list.map((x) => HalfTime.fromMap(x)));
+  }
 }
