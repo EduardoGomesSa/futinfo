54d79dc08e0087f3100366f55c7db895861369e8
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-04 14:51:52 -0300

    Criação dos métodos de conversão MatchModel

--- lib/src/models/match_model.dart
+++ lib/src/models/match_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:futinfo/src/models/team_model.dart';
 import 'package:futinfo/src/models/value_objects/score_model.dart';
 
@@ -23,4 +25,35 @@ class MatchModel {
     required this.awayTeam,
     required this.score,
   });
+
+  factory MatchModel.fromMap(Map<String, dynamic> map) {
+    return MatchModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      utcDate: map['utcDate'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['utcDate'] as int)
+          : null,
+      status: map['status'] != null ? map['status'] as String : null,
+      matchday: map['matchday'] != null ? map['matchday'] as int : null,
+      stage: map['stage'] != null ? map['stage'] as String : null,
+      lastUpdated: map['lastUpdated'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['lastUpdated'] as int)
+          : null,
+      homeTeam: map['homeTeam'] != null
+          ? TeamModel.fromMap(map['homeTeam'] as Map<String, dynamic>)
+          : null,
+      awayTeam: map['awayTeam'] != null
+          ? TeamModel.fromMap(map['awayTeam'] as Map<String, dynamic>)
+          : null,
+      score: map['score'] != null
+          ? ScoreModel.fromMap(map['score'] as Map<String, dynamic>)
+          : null,
+    );
+  }
+
+  factory MatchModel.fromJson(String source) =>
+      MatchModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  static List<MatchModel> fromList(list) {
+    return List<MatchModel>.from(list.map((x) => MatchModel.fromMap(x)));
+  }
 }
