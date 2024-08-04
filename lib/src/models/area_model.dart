a31511b6f55f143d4227a428b94b3f1f4078ea28
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-03 22:47:28 -0300

    Criação dos métodos de conversão de AreaModel

--- lib/src/models/area_model.dart
+++ lib/src/models/area_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class AreaModel {
   final int? id;
   final String? name;
@@ -10,4 +12,20 @@ class AreaModel {
     required this.code,
     required this.flag,
   });
+
+  factory AreaModel.fromMap(Map<String, dynamic> map) {
+    return AreaModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      code: map['code'] != null ? map['code'] as String : null,
+      flag: map['flag'] != null ? map['flag'] as String : null,
+    );
+  }
+
+  factory AreaModel.fromJson(String source) =>
+      AreaModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  static List<AreaModel> fromList(list) {
+    return List<AreaModel>.from(list.map((x) => AreaModel.fromMap(x)));
+  }
 }
