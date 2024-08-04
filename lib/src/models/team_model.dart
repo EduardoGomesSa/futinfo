242235f7384acf1ec7520fd2ee004f434a5fe26f
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-04 01:05:44 -0300

    Criação dos métodos de conversão de TeamModel

--- lib/src/models/team_model.dart
+++ lib/src/models/team_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class TeamModel {
   final int? id;
   final String? name;
@@ -12,4 +14,21 @@ class TeamModel {
     required this.tla,
     required this.crest,
   });
+
+  factory TeamModel.fromMap(Map<String, dynamic> map) {
+    return TeamModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      shortName: map['shortName'] != null ? map['shortName'] as String : null,
+      tla: map['tla'] != null ? map['tla'] as String : null,
+      crest: map['crest'] != null ? map['crest'] as String : null,
+    );
+  }
+
+  factory TeamModel.fromJson(String source) =>
+      TeamModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  static List<TeamModel> fromList(list) {
+    return List<TeamModel>.from(list.map((x) => TeamModel.fromMap(x)));
+  }
 }
