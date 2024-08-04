8e346bb492399787871d279e5290e9f433cd693c
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-03 23:09:55 -0300

    Criação dos métodos de conversão de SeasonModel e CompetitionModel

--- lib/src/models/competition_model.dart
+++ lib/src/models/competition_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class CompetitionModel {
   final int? id;
   final String? name;
@@ -12,4 +14,22 @@ class CompetitionModel {
     required this.type,
     required this.emblem,
   });
+
+  factory CompetitionModel.fromMap(Map<String, dynamic> map) {
+    return CompetitionModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      code: map['code'] != null ? map['code'] as String : null,
+      type: map['type'] != null ? map['type'] as String : null,
+      emblem: map['emblem'] != null ? map['emblem'] as String : null,
+    );
+  }
+
+  factory CompetitionModel.fromJson(String source) =>
+      CompetitionModel.fromMap(jsonDecode(source) as Map<String, dynamic>);
+
+  static List<CompetitionModel> fromList(list) {
+    return List<CompetitionModel>.from(
+        list.map((x) => CompetitionModel.fromMap(x)));
+  }
 }
--- lib/src/models/season_model.dart
+++ lib/src/models/season_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class SeasonModel {
   final int? id;
   final DateTime? startDate;
@@ -12,4 +14,26 @@ class SeasonModel {
     required this.currentMatchday,
     required this.winner,
   });
+
+  factory SeasonModel.fromMap(Map<String, dynamic> map) {
+    return SeasonModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      startDate: map['startDate'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['startDate'] as int)
+          : null,
+      endDate: map['endDate'] != null
+          ? DateTime.fromMillisecondsSinceEpoch(map['endDate'] as int)
+          : null,
+      currentMatchday:
+          map['currentMatchday'] != null ? map['currentMatchday'] as int : null,
+      winner: map['winner'] != null ? map['winner'] as String : null,
+    );
+  }
+
+  factory SeasonModel.fromJson(String source) =>
+      SeasonModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  static List<SeasonModel> fromList(list) {
+    return List<SeasonModel>.from(list.map((x) => SeasonModel.fromMap(x)));
+  }
 }
