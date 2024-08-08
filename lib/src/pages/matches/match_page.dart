a1068f398959021794deb3acf0d652231ec533f5
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-07 23:46:17 -0300

    Matchpage parcialmente completa

--- lib/src/pages/matches/match_page.dart
+++ lib/src/pages/matches/match_page.dart
@@ -14,43 +14,152 @@ class MatchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Jogo"),
-      ),
-      body: Column(
-          children: [
-            Text(appUtils.formatDateTimeHour(model.utcDate!)),
-            Text(appUtils.formatDateTime(model.utcDate!)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(model.homeTeam!.crest!),
-                  radius: 15,
-                ),
-                const SizedBox(width: 8),
-                Text("${model.homeTeam!.tla} "),
-                const SizedBox(width: 8),
-                model.score!.fullTime!.home != null
-                    ? Text(model.score!.fullTime!.home.toString())
-                    : const Text(" -"),
-                const SizedBox(width: 8),
-                const Text(" X "),
-                const SizedBox(width: 8),
-                model.score!.fullTime!.away != null
-                    ? Text(model.score!.fullTime!.away.toString())
-                    : const Text("- "),
-                const SizedBox(width: 8),
-                Text(" ${model.awayTeam!.tla!}"),
-                const SizedBox(width: 8),
-                CircleAvatar(
-                  backgroundImage: NetworkImage(model.awayTeam!.crest!),
-                  radius: 15,
-                ),
-              ],
-            )
-          ],
+        appBar: AppBar(
+          title: const Text("Jogo"),
         ),
-    );
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 10),
+              child: Card(
+                color: Colors.white.withAlpha(100),
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      left: 20, right: 20, top: 20, bottom: 20),
+                  child: Column(
+                    children: [
+                      Text(appUtils.formatDateTimeHour(model.utcDate!)),
+                      Text(appUtils.formatDateTime(model.utcDate!)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.homeTeam!.crest!),
+                            radius: 15,
+                          ),
+                          const SizedBox(width: 8),
+                          Text("${model.homeTeam!.tla} "),
+                          const SizedBox(width: 8),
+                          model.score!.fullTime!.home != null
+                              ? Text(model.score!.fullTime!.home.toString())
+                              : const Text(" -"),
+                          const SizedBox(width: 8),
+                          const Text(" X "),
+                          const SizedBox(width: 8),
+                          model.score!.fullTime!.away != null
+                              ? Text(model.score!.fullTime!.away.toString())
+                              : const Text("- "),
+                          const SizedBox(width: 8),
+                          Text(" ${model.awayTeam!.tla!}"),
+                          const SizedBox(width: 8),
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.awayTeam!.crest!),
+                            radius: 15,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Placar"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0),
+              child: Card(
+                color: Colors.white.withAlpha(100),
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      left: 20, right: 20, top: 20, bottom: 20),
+                  child: Column(
+                    children: [
+                      const Text("Primeiro Tempo"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.homeTeam!.crest!),
+                            radius: 15,
+                          ),
+                          const SizedBox(width: 8),
+                          Text("${model.homeTeam!.tla} "),
+                          const SizedBox(width: 8),
+                          model.score!.halfTime!.home != null
+                              ? Text(model.score!.halfTime!.home.toString())
+                              : const Text(" -"),
+                          const SizedBox(width: 8),
+                          const Text(" X "),
+                          const SizedBox(width: 8),
+                          model.score!.halfTime!.away != null
+                              ? Text(model.score!.halfTime!.away.toString())
+                              : const Text("- "),
+                          const SizedBox(width: 8),
+                          Text(" ${model.awayTeam!.tla!}"),
+                          const SizedBox(width: 8),
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.awayTeam!.crest!),
+                            radius: 15,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8, top: 10),
+              child: Card(
+                color: Colors.white.withAlpha(100),
+                child: Container(
+                  margin: const EdgeInsets.only(
+                      left: 20, right: 20, top: 20, bottom: 20),
+                  child: Column(
+                    children: [
+                      const Text("Segundo Tempo"),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.homeTeam!.crest!),
+                            radius: 15,
+                          ),
+                          const SizedBox(width: 8),
+                          Text("${model.homeTeam!.tla} "),
+                          const SizedBox(width: 8),
+                          model.score!.fullTime!.home != null
+                              ? Text(model.score!.fullTime!.home.toString())
+                              : const Text(" -"),
+                          const SizedBox(width: 8),
+                          const Text(" X "),
+                          const SizedBox(width: 8),
+                          model.score!.fullTime!.away != null
+                              ? Text(model.score!.fullTime!.away.toString())
+                              : const Text("- "),
+                          const SizedBox(width: 8),
+                          Text(" ${model.awayTeam!.tla!}"),
+                          const SizedBox(width: 8),
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(model.awayTeam!.crest!),
+                            radius: 15,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
