9f850d2c4c0884b9c9a42e91875851520f3cb32b
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-08-11 22:22:37 -0300

    Criação do CoachModel

---
+++ lib/src/models/coach_model.dart
@@ -0,0 +1,48 @@
+class CoachModel {
+  int? id;
+  String? firstName;
+  String? lastName;
+  String? name;
+  DateTime? dateOfBirth;
+  String? nationality;
+  Contract? contract;
+
+  CoachModel({
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.name,
+    this.dateOfBirth,
+    this.nationality,
+    this.contract,
+  });
+
+  factory CoachModel.fromMap(Map<String, dynamic> map) {
+    return CoachModel(
+      id: map['id'] != null ? map['id'] as int : null,
+      firstName: map['firstName'] != null ? map['firstName'] as String : null,
+      lastName: map['lastName'] != null ? map['lastName'] as String : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      dateOfBirth: map['dateOfBirth'] != null ? DateTime.parse(map['dateOfBirth']) : null,
+      nationality: map['nationality'] != null ? map['nationality'] as String : null,
+      contract: map['contract'] != null ? Contract.fromMap(map['contract'] as Map<String, dynamic>) : null,       
+    );
+  }
+}
+
+class Contract {
+  DateTime? start;
+  DateTime? until;
+
+  Contract({
+    this.start,
+    this.until,
+  });
+
+  factory Contract.fromMap(Map<String, dynamic> map) {
+    return Contract(
+      start: map['start'] != null ? DateTime.parse(map['start']) : null,
+      until: map['until'] != null ? DateTime.parse(map['until']) : null,
+    );
+  }
+}
