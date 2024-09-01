beebc2e5d5849313206339751b547ee0bbcfd91b
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-01 16:42:59 -0300

    Criação do tableScorerModel

---
+++ lib/src/models/table_scorer_model.dart
@@ -0,0 +1,16 @@
+import 'package:futinfo/src/models/value_objects/scorer.dart';
+
+class TableScorerModel {
+  List<Scorer>? scorers;
+
+  TableScorerModel({this.scorers});
+
+  factory TableScorerModel.fromMap(Map<String, dynamic> map) {
+    return TableScorerModel(
+        scorers: map['scorers'] != null
+            ? List<Scorer>.from((map['scorers'] as List<dynamic>).map<Scorer?>(
+                (scorer) =>
+                    Scorer.fromMap(map['scorers'] as Map<String, dynamic>)))
+            : null);
+  }
+}
